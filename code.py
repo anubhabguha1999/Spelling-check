8dd2ee54b46ef5b4e07eea55190c54fc8283dfa8
Anubhab Guha <96384072+anubhabguha1999@users.noreply.github.com>
2023-02-05 00:05:25 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,17 @@
+# pip install textblob
+
+from textblob import TextBlob
+
+words = ["Data Scence", "Mahine learnin"]
+
+corrected_words = []
+
+for i in words:
+    corrected_words.append(TextBlob(i))
+
+print("Worng Words : ", words)
+
+print("Corrected Words are : ")
+
+for i in corrected_words:
+    print(i.correct(), end=" ")
